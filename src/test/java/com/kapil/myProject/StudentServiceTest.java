4cf61165e77a6bf1f9afc8bfa4e42ef04ef69a99
kapil576700 <kapil576700@gmail.com>
2022-03-17 22:56:27 -0400

    Updating Docker

--- src/test/java/com/kapil/myProject/StudentServiceTest.java
+++ src/test/java/com/kapil/myProject/StudentServiceTest.java
@@ -1,21 +1,20 @@
 package com.kapil.myProject;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
+import com.kapil.myProject.Entity.Student;
+import com.kapil.myProject.Repository.StudentRepo;
+import com.kapil.myProject.Repository.StudentService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import com.kapil.myProject.Entity.Student;
-import com.kapil.myProject.Repository.StudentRepo;
-import com.kapil.myProject.Repository.StudentService;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StudentServiceTest {
@@ -35,6 +34,29 @@ public class StudentServiceTest {
 		when(repo.findAll()).thenReturn(expectedList);
 		List<Student> actualList = service.findStudents();
         Mockito.verify(repo, Mockito.times(1)).findAll();
+	}
 
+	@Test
+	public void testSaveStudent(){
+		Student student = new Student(104,"Jessica","A");
+		service.saveStudent(student);
+		Mockito.verify(repo, Mockito.times(1)).save(student);
+	}
+
+	@Test
+	public void testDeleteStudent(){
+		Student student = new Student(104,"Jessica","A");
+		when(repo.getById(student.getId())).thenReturn(student);
+		Student s = repo.getById(student.getId());
+		Mockito.verify(repo,Mockito.times(1)).getById(student.getId());
+		service.deleteStudent(student.getId());
+		Mockito.verify(repo,Mockito.times(1)).delete(s);
+	}
+
+	@Test
+	public void testUpdateStudent(){
+		Student student = new Student(104,"Jessica","A");
+		service.saveStudent(student);
+		Mockito.verify(repo, Mockito.times(1)).save(student);
 	}
 }
